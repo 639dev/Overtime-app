24a331f795e6148f51f5759ae3f05ee42a3d74c8
639dev <gharood_3@hotmail.com>
2017-05-28 00:29:19 +0300

    implemented full name method for users

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  before do
+    @user = User.create(email: "test@test.com" ,password: "stfstf" ,password_confirmation: "stfstf" ,first_name: "John" ,last_name: "Mac")
+  end
+
   describe "creation" do
     before do
       @post = Post.create(date: Date.today , rationale: "anything")
@@ -13,6 +17,12 @@ RSpec.describe Post, type: :model do
       @post.date = nil
       @post.rationale = nil
       expect(@post).to_not be_valid
+    end
+  end
+
+  describe 'custom name methods' do
+    it 'has a full name method that combines first and last name' do
+      expect(@user.full_name).to eq('MAC, JOHN')
     end
   end
 end
