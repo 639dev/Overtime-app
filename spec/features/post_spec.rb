53d4127100cd4f1b6c77fc8f64a709568d122a92
639dev <gharood_3@hotmail.com>
2017-05-27 03:07:34 +0300

    set new,create,show logic to posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -7,10 +7,27 @@ describe 'navigate' do
       expect(page.status_code).to eq(200)
     end
 
-    it 'it has a title pf post' do
+    it 'it has a title of post' do
       visit posts_path
       expect(page).to have_content(/Posts/)
     end
+  end
 
+  describe "creation" do
+    it "has a new form that can be reached" do
+      visit new_post_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it "can be created from new form page" do
+      visit new_post_path
+
+      fill_in 'post[date]',with: Date.today
+      fill_in 'post[rationale]',with: "some rationale"
+
+      click_on "Save"
+
+      expect(page).to have_content("some rationale")
+    end
   end
 end
