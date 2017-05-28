cc7caa6d7a01e92d52ebe529d11a94fe16028dcc
639dev <gharood_3@hotmail.com>
2017-05-29 02:45:20 +0300

    added new post button to navbar

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -27,6 +27,15 @@ describe 'navigate' do
     end
   end
 
+  describe 'new' do
+    it 'has a link from the homepage' do
+    visit root_path
+    click_link("new post from nav")
+    expect(page.status_code).to eq(200)
+    end
+  end
+
+
   describe "creation" do
     before do
       visit new_post_path
@@ -76,4 +85,6 @@ describe 'navigate' do
    
     end
   end
+
+
 end
