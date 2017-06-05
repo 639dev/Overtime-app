e9a6f447f818f88e18d40bd0215fecfc45205ddf
639dev <gharood_3@hotmail.com>
2017-06-05 22:43:23 +0300

    blocked non admin usrs from accessing index page of audit

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -19,5 +19,13 @@ describe 'AudiLog feature' do
       visit audit_logs_path
       expect(page).to have_content(/MAC, JOHN/)
     end
+
+    it 'cannot be accessed by non admin users' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user,:scope => :user)
+      visit audit_logs_path
+      expect(current_path).to eq(root_path)
+    end
   end
 end
