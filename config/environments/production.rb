c66bc7b0216ba5aa3a18c13b516d58b32422d216
639dev <gharood_3@hotmail.com>
2017-07-03 23:53:33 +0300

    edit production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,8 @@ Rails.application.configure do  config.cache_classes = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.assets.js_compressor = :uglifier  config.assets.compile = false
+  config.assets.js_compressor = :uglifier 
+  config.assets.compile = true
   config.assets.digest = true
   config.log_level = :debug
   config.i18n.fallbacks = true
@@ -11,7 +12,6 @@ Rails.application.configure do  config.cache_classes = true
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
   config.serve_static_assets = true
-  config.assets.compile = true
 end
 
 
