6fb3b6bbe1bd4c91c3843ff31ca2855f75df0ab6
639dev <gharood_3@hotmail.com>
2017-07-03 22:58:35 +0300

    updated the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,10 +24,10 @@ AuditLog.create!(user_id: @user.id,status: 0,strat_date: (Date.today - 20.days))
 puts "3 audit logs have been created"
 
 
-100.times do |post|
+10.times do |post|
   Post.create!(date: Date.yesterday, rationale: "#{post} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",user_id: @user.id,overtime_request: 2.5)
 end
 
-puts "100 post have been created"
+puts "10 post have been created"
 
 
