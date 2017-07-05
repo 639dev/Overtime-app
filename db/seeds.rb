63f840850cf21cfd0e41da5feabe017782d837e2
639dev <gharood_3@hotmail.com>
2017-07-06 01:24:22 +0300

    build out rake task for sending sms and STI for imployees

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-@user = User.create!(email: "tagreedalshammari@gmail.com" ,
+@employee = Employee.create!(email: "tagreedalshammari@gmail.com" ,
   password: "stfstf" ,
   password_confirmation: "stfstf" ,
   first_name: "john" ,
   last_name: "mac",
   phone: "1312948956")
 
-puts '1 user created'
+puts '1 employee created'
 
-AdminUser.create!(email: "gharood_3@hotmail.com" ,
+AdminUser.create!(email: "admin@devcamp.com" ,
   password: "stfstf" ,
   password_confirmation: "stfstf" ,
   first_name: "tagreed" ,
@@ -16,16 +16,16 @@ AdminUser.create!(email: "gharood_3@hotmail.com" ,
 
 puts '1 admin user created'
 
-AuditLog.create!(user_id: @user.id,status: 0,strat_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id,status: 0,strat_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id,status: 0,strat_date: (Date.today - 20.days))
+AuditLog.create!(user_id: @employee.id,status: 0,strat_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @employee.id,status: 0,strat_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @employee.id,status: 0,strat_date: (Date.today - 20.days))
 
 
 puts "3 audit logs have been created"
 
 
 10.times do |post|
-  Post.create!(date: Date.yesterday, rationale: "#{post} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",user_id: @user.id,overtime_request: 2.5)
+  Post.create!(date: Date.yesterday, rationale: "#{post} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",user_id: @employee.id,overtime_request: 2.5)
 end
 
 puts "10 post have been created"
--- lib/tasks/notification.rake
+++ lib/tasks/notification.rake
@@ -1,14 +1,20 @@
 namespace :notification do
   desc "sends sms notification to employees asking them to log if they have overtime"
   task sms: :environment do
-    # 1. Scheduele to run at sunday at 5pm.
-    # 2. Iterate over all employees.
-    # 3. Skip admin users.
-    # 4. Send a message that has instructions and a link to log time.
-    #User.all.each do |user|
-    #  SmsTool.send_sms(number: 0000)
-    #end
-    
+    if Time.now.sunday?
+      msg = "Please log into the overtime managment dashboard to request overtime or confirm your hours for last week"
+      # 1. Scheduele to run at sunday at 5pm.
+      # 2. Iterate over all employees.
+      # 3. Skip admin users.
+      # 4. Send a message that has instructions and a link to log time.
+      #User.all.each do |user|
+      #  SmsTool.send_sms(number: 0000)
+      #end
+      employee = Employee.all?
+        employee.each do |employee|
+          SmsTool.send_sms(number: employee.phone,message: msg)
+        end
+    end 
   end
 
 
